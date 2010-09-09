6864a9bbf044ad02b409a1b4abaf6c01db49c9bc
Nigjo <Nigjo@dbaf118e-4728-efd9-8ef1-a7cade6a4660>
2010-09-09 12:40:47 +0000

    Observing changes in FileSystem (add, remove, modify) and wrap as property change events to BundleGroup.

--- SuiteBundles/src/com/kenai/suitetranslator/bundlenode/data/BundleGroup.java
+++ SuiteBundles/src/com/kenai/suitetranslator/bundlenode/data/BundleGroup.java
@@ -1,5 +1,6 @@
 package com.kenai.suitetranslator.bundlenode.data;
 
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -8,20 +9,26 @@ import java.util.Locale;
 import org.openide.filesystems.FileObject;
 
 /**
- * Daten eines Bundles
+ * Daten eines Bundles.
  *
  * @todo Hier fehlt die Beschreibung der Klasse.
  *
- * @author hof
+ * @author nigjo
  */
 public class BundleGroup implements Iterable<BundleFile>
 {
+  public static final String PROP_BUNDLE_DIR = "BundleGroup.bundleDir";
+  public static final String PROP_LOCALE_COUNT = "BundleGroup.localeCount";
+  public static final String PROP_LAST_CHANGED = "BundleGroup.lastChanged";
   List<BundleFile> files;
   private final String basename;
+  private BundleGroupObserver changeObserver;
+  private long lastChanged;
 
   public BundleGroup(String basename)
   {
     this.basename = basename;
+    changeObserver = new BundleGroupObserver(this);
   }
 
   public String getBasename()
@@ -33,7 +40,25 @@ public class BundleGroup implements Iterable<BundleFile>
   {
     if(files == null)
       files = new ArrayList<BundleFile>();
-    files.add(new BundleFile(this, subfile));
+    BundleFile bundleFile = new BundleFile(this, subfile);
+    files.add(bundleFile);
+    changeObserver.bundleLocaleAdded(bundleFile);
+  }
+
+  public void remove(FileObject deletedFile)
+  {
+    if(files == null)
+      return;
+    remove(getFile(deletedFile));
+  }
+
+  void remove(BundleFile bundleFile)
+  {
+    if(bundleFile == null)
+      return;
+    int index = files.indexOf(bundleFile);
+    files.remove(index);
+    changeObserver.bundleLocaleRemoved(bundleFile);
   }
 
   @Override
@@ -65,6 +90,16 @@ public class BundleGroup implements Iterable<BundleFile>
       b.append(")");
     }
     return b.toString();
+  }
+
+  private BundleFile getFile(FileObject fo)
+  {
+    for(BundleFile file : this)
+    {
+      if(file.getFile().equals(fo))
+        return file;
+    }
+    return null;
   }
 
   public BundleFile getFile(Locale locale)
@@ -106,6 +141,7 @@ public class BundleGroup implements Iterable<BundleFile>
     return files.size();
   }
 
+  // <editor-fold defaultstate="collapsed" desc="Dummy group/WaitNode">
   public boolean isDummyGroup()
   {
     return this instanceof DummyBundleGroup;
@@ -116,6 +152,15 @@ public class BundleGroup implements Iterable<BundleFile>
     return new DummyBundleGroup();
   }
 
+  void setLastChange(long time)
+  {
+    long lastChange = this.lastChanged;
+    if(time < lastChange)
+      return;
+    this.lastChanged = time;
+    changeObserver.bundleChanged(lastChange, time);
+  }
+
   private static class DummyBundleGroup extends BundleGroup
   {
     public DummyBundleGroup()
@@ -123,5 +168,36 @@ public class BundleGroup implements Iterable<BundleFile>
       super(DummyBundleGroup.class.getName());
     }
 
+    @Override
+    public void add(FileObject subfile)
+    {
+      throw new UnsupportedOperationException("only dummy"); //NOI18N
+    }
+
+  }// </editor-fold>
+
+  // <editor-fold defaultstate="collapsed" desc="propertyChangeSupport">
+  public synchronized void removePropertyChangeListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+    changeObserver.removePropertyChangeListener(propertyName, listener);
   }
+
+  public synchronized boolean hasListeners(String propertyName)
+  {
+    return changeObserver.hasListeners(propertyName);
+  }
+
+  public synchronized PropertyChangeListener[] getPropertyChangeListeners(
+      String propertyName)
+  {
+    return changeObserver.getPropertyChangeListeners(propertyName);
+  }
+
+  public synchronized void addPropertyChangeListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+    changeObserver.addPropertyChangeListener(propertyName, listener);
+  }
+  // </editor-fold>
 }
