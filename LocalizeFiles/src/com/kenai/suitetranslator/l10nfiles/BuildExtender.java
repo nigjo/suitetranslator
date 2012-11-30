c11272a49e120159de786716184ee7abe4d7bec5
Nigjo <Nigjo@dbaf118e-4728-efd9-8ef1-a7cade6a4660>
2012-11-30 14:28:10 +0000

    Imported a first try of a "select-resources-for-localization"-module

---
+++ LocalizeFiles/src/com/kenai/suitetranslator/l10nfiles/BuildExtender.java
@@ -0,0 +1,12 @@
+package com.kenai.suitetranslator.l10nfiles;
+
+/**
+ * Eine neue Klasse von hof. Erstellt Nov 30, 2012, 3:07:58 PM.
+ *
+ * @todo Hier fehlt die Beschreibung der Klasse.
+ *
+ * @author hof
+ */
+public class BuildExtender {
+
+}
